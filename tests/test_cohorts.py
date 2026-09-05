44b48f278937d428039586aa01a0af8abfa118bb
Ruben Patanè <ruben.patane@gmail.com>
2026-09-05 13:59:41 +0200

    test(model): cover centralized Boeing cohort mapping

---
+++ tests/test_cohorts.py
@@ -0,0 +1,15 @@
+from bsfm.cohorts import boeing_cohort
+
+
+def test_boeing_cohort_maps_supported_families():
+    assert boeing_cohort('737-800')=='737-NG'
+    assert boeing_cohort('737-400')=='737-Classic'
+    assert boeing_cohort('737 MAX 8')=='737-MAX'
+    assert boeing_cohort('737-8')=='737-MAX'
+    assert boeing_cohort('777-300ER')=='777'
+    assert boeing_cohort('787-9')=='787'
+
+
+def test_boeing_cohort_fails_closed_for_unknown_model():
+    assert boeing_cohort('A320') is None
+    assert boeing_cohort('') is None
